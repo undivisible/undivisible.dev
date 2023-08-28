5e16cf3995d5605143403578be32397d21b918ca
undivisible <vms.fang.vortexmemesquad@gmail.com>
2023-08-28 13:44:48 +1000

    the funny should work.
    it didn't last time.

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
 
-if($_POST["message"]) {
-  $message = $_POST["message"];
-  mail("you@example.com", "Contact Form", "\"$message\" from your site"); 
-}
+//if($_POST["message"]) {
+//  $message = $_POST["message"];
+//  mail("you@example.com", "Contact Form", "\"$message\" from your site"); 
+//}
 
 ?>
