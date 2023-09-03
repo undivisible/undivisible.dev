90dfe16ef607b161704594e59c66c7b9ccac7c5a
undivisible <vms.fang.vortexmemesquad@gmail.com>
2023-09-03 14:43:58 +1000

    fffffffffffffffffffffff

--- script.js
+++ script.js
@@ -44,3 +44,21 @@ window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-3YW9C78ZXL');
+
+window.onload = () => {
+    location.hash = '#toggle';
+    };
+
+document.body.addEventListener('click', () => {
+
+    let hash = location.hash;
+    
+    if(hash == '#toggle') {
+        location.hash = '#toggle2';
+    } else if (hash == '#toggle2') {
+        location.hash = '#toggle';
+    } else {
+        location.hash = '#toggle';
+    }
+    
+    });
