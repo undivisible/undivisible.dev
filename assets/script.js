982c7117bde06e2a5be71c9eaf7b58962e400b40
undivisible <vms.fang.vortexmemesquad@gmail.com>
2023-09-12 20:31:50 +1000

    the language update

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+
 window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
@@ -7,15 +8,32 @@ window.onload = () => {
     location.hash = '#toggle';
     };
 
+var usrlang = (navigator.language || navigator.userLanguage).toLowerCase();
+
+if (usrlang === "ru") {
+    window.location = "ru/index.html";
+} 
+else if (usrlang === "id") {
+    window.location = "id/index.html";
+} 
+else if (usrlang === "zh") {
+    window.location = "zh/index.html";
+} 
+else {
+    return;
+}
+
 document.body.addEventListener('click', () => {
 
     let hash = location.hash;
     
     if(hash == '#toggle') {
         location.hash = '#toggle2';
-    } else if (hash == '#toggle2') {
+    } 
+    else if (hash == '#toggle2') {
         location.hash = '#toggle';
-    } else {
+    } 
+    else {
         location.hash = '#toggle';
     }
     
