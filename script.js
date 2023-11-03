7f1710aa0538df0ea4dff7eaa5f41b3901206efe
undivisible <vms.fang.vortexmemesquad@gmail.com>
2023-11-03 14:23:52 +1100

    optimizations

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
-(function () {
-    var names = ["max","mack","maxim","maximus","祁明思","максим" ],
-    i = 0;
-    setInterval(function(){ $('#names').fadeOut(function(){
-        $(this).html(names[(i = (i + 1) % names.length)]).fadeIn();
-      }); }, 3000)
-    var greetings = ["hello!","здравствуй!","你好！","hey!","доброе день" ],
-    i2 = 0;
-    setInterval(function(){ $('#greetings').fadeOut(function(){
-        $(this).html(greetings[(i2 = (i2 + 1) % greetings.length)]).fadeIn();
-      }); }, 3000)
-    var jobs = ["student","developer","entrepreneur","video editor" ],
-    i3 = 0;
-    setInterval(function(){ $('#jobs').fadeOut(function(){
-        $(this).html(jobs[(i3 = (i3 + 1) % jobs.length)]).fadeIn();
-    }); }, 3000)
-    var langs = ["english","russian","cantonese","chinese","indonesian" ],
-    i4 = 0;
-    setInterval(function(){ $('#langs').fadeOut(function(){
-        $(this).html(langs[(i4 = (i4 + 1) % langs.length)]).fadeIn();
-    }); }, 3000)
-    var code = ["html and css","javascript","python","c#" ],
-    i5 = 0;
-    setInterval(function(){ $('#code').fadeOut(function(){
-        $(this).html(code[(i5 = (i5 + 1) % code.length)]).fadeIn();
-    }); }, 3000)
-})();
-
 function mouseover() {
-    const letters = "abcdefghijklmnopqrstuvwxyz";
-    const link = document.querySelectorAll("#link");
+   
 
+    
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  const text = document.getElementById("text");
+  const links = document.getElementById("links");
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const link = document.querySelectorAll("#link");
+  
+  function updateText() {
+    (function () {
+        var names = ["max","mack","maxim","maximus","祁明思","максим" ],
+        i = 0;
+        setInterval(function(){ $('#names').fadeOut(function(){
+            $(this).html(names[(i = (i + 1) % names.length)]).fadeIn();
+          }); }, 3000)
+        var greetings = ["hello!","здравствуй!","你好！","hey!","доброе день" ],
+        i2 = 0;
+        setInterval(function(){ $('#greetings').fadeOut(function(){
+            $(this).html(greetings[(i2 = (i2 + 1) % greetings.length)]).fadeIn();
+          }); }, 3000)
+        var jobs = ["student","developer","entrepreneur","video editor" ],
+        i3 = 0;
+        setInterval(function(){ $('#jobs').fadeOut(function(){
+            $(this).html(jobs[(i3 = (i3 + 1) % jobs.length)]).fadeIn();
+        }); }, 3000)
+        var langs = ["english","russian","cantonese","chinese","indonesian" ],
+        i4 = 0;
+        setInterval(function(){ $('#langs').fadeOut(function(){
+            $(this).html(langs[(i4 = (i4 + 1) % langs.length)]).fadeIn();
+        }); }, 3000)
+        var code = ["html and css","javascript","python","c#" ],
+        i5 = 0;
+        setInterval(function(){ $('#code').fadeOut(function(){
+            $(this).html(code[(i5 = (i5 + 1) % code.length)]).fadeIn();
+        }); }, 3000)
+    })();
+  }
+
+  function updateLinks() {
+    links.addEventListener("mouseover", function(){
+        text.style.animation = "linksr 0.5s ease-in forwards";
+        links.style.animation = "links 0.5s ease-in forwards";
+    });
+
+    links.addEventListener("mouseout", function(){
+        text.style.animation = "links 0.5s ease-out forwards";
+        links.style.animation = "linksr 0.5s ease-out forwards";
+    });
+  }
+
+  function updateMouseOver() {
     link.forEach(link => {
         let interval = null;
 
@@ -58,19 +82,10 @@ function mouseover() {
             }, 30);
         }
     });
-}
+  }
 
-function anim() {
-    const text = document.getElementById('text');
-    const links = document.getElementById('links');
-
-    links.addEventListener("mouseover", function(){
-        text.style.animation = "linksr 0.5s ease-in forwards";
-        links.style.animation = "links 0.5s ease-in forwards";
-    });
-
-    links.addEventListener("mouseout", function(){
-        text.style.animation = "links 0.5s ease-out forwards";
-        links.style.animation = "linksr 0.5s ease-out forwards";
-    });
-}
+  updateText();
+  updateLinks();
+  updateMouseOver();
+  
+});
