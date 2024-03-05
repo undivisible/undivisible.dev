71850c23941b4c0e6541ea0f5e52928f126798de
undivisible <vms.fang.vortexmemesquad@gmail.com>
2024-03-06 09:21:36 +1100

    Update script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
   
   function updateText() {
     (function () {
-        var names = ["max","mack","maxim","maximus","祁明思","максим" ],
+        var names = ["max","mack","maxim","maximus","maximillian","祁明思","максим", ],
         i = 0;
         setInterval(function(){ $('#names').fadeOut(function(){
             $(this).html(names[(i = (i + 1) % names.length)]).fadeIn();
           }); }, 3000)
-        var greetings = ["hello!","здравствуй!","你好！","hey!","доброе день" ],
+        var greetings = ["hello!","дарова!","哈喽！","hey!","добрый день","你好！","heyo","привет!","喂!" ],
         i2 = 0;
         setInterval(function(){ $('#greetings').fadeOut(function(){
             $(this).html(greetings[(i2 = (i2 + 1) % greetings.length)]).fadeIn();
@@ -28,12 +28,12 @@ document.addEventListener("DOMContentLoaded", function() {
         setInterval(function(){ $('#jobs').fadeOut(function(){
             $(this).html(jobs[(i3 = (i3 + 1) % jobs.length)]).fadeIn();
         }); }, 3000)
-        var langs = ["english","russian","cantonese","chinese","indonesian" ],
+        var langs = ["cantonese","english","cantonese","mandarin","indonesian"],
         i4 = 0;
         setInterval(function(){ $('#langs').fadeOut(function(){
             $(this).html(langs[(i4 = (i4 + 1) % langs.length)]).fadeIn();
         }); }, 3000)
-        var code = ["html and css","javascript","python","c#" ],
+        var code = ["typescript","python","c#","javascipt","html+css"],
         i5 = 0;
         setInterval(function(){ $('#code').fadeOut(function(){
             $(this).html(code[(i5 = (i5 + 1) % code.length)]).fadeIn();
